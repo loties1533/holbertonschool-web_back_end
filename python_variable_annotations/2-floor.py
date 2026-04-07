684f9bd6dae9413986d44fc1e09aaf3b62d5911b
loties1533 <alexislaubert@yahoo.fr>
2026-04-07 11:47:10 +0000

    fonction pour recup le nombre entier d un float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module pour fonction floor
+"""
+import math
+
+
+def floor(n:  float) -> int:
+    """
+    renvoie l entier inférieure (floor) d'un flottant ( ex = 3.14 -> 3)
+    """
+    return math.floor(n)
