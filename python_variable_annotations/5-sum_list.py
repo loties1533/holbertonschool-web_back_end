30da1b71890070108e35bb689ec73c68f3deebfa
loties1533 <alexislaubert@yahoo.fr>
2026-04-07 21:47:08 +0000

    réation de sum_list pour additionner  une liste de floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module avec fonction pour additionner une liste de floats.
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    retourne la somme d'une liste de float
+    """
+
+    return sum(input_list)
