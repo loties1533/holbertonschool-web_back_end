2f49d8b3abee6f39fc860bbe85c6835349037f2a
loties1533 <alexislaubert@yahoo.fr>
2026-04-07 09:27:24 +0000

    concatene str1 et str2

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module de base pour les annotations de variable
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    additionne les 2 floats
+    """
+    return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Ce module contient une fonction qui concatene deux chaines de caracteres
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatene deux chaines de caracteres (str1 et str2)
+
+    Args:
+
+        str1 (str)
+        str2 (str)
+
+    Returns:
+        str = str1 + str2
+    """
+    return str1 + str2
