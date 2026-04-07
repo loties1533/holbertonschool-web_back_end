8963b0c7aa80fa7b9c850d1601c8fb01bc021956
loties1533 <alexislaubert@yahoo.fr>
2026-04-07 12:04:06 +0000

    prend un float en renvoie une version texte (str)

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module pour fonction to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    prend un float en renvoie une version texte (str)
+    """
+    return str(n)
