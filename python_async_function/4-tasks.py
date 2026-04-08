7d2607cfbc3911545052223b019e14a782f7efd7
loties <alexislaubert@yahoo.fr>
2026-04-08 19:56:06 +0200

    pycodestyle valide

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,8 +7,10 @@ import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """execute n task_wait_random en meme temps et retourne les delais dans l ordre d execution
+    """execute n task_wait_random en meme temps et retourne
+      les delais dans l ordre d execution
     """
     tasks = []
     ma_list = []
