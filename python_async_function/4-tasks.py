8797912e3b6555b59c540270554081b73e029255
loties <alexislaubert@yahoo.fr>
2026-04-08 19:54:48 +0200

    execute 'n' tache en mm temp  pour gagner du temp , utilise 'tasks' pour le travail en cour  et ' ma_list' pour les resultats

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+module qui execute plusieurs task en meme temps
+"""
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """execute n task_wait_random en meme temps et retourne les delais dans l ordre d execution
+    """
+    tasks = []
+    ma_list = []
+    for i in range(n):
+        task = task_wait_random(max_delay)
+        tasks.append(task)
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        ma_list.append(delay)
+    return ma_list
