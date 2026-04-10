b8e1d88264d9dffbe35ef31e85bd3fcb1c8b2a99
loties <alexislaubert@yahoo.fr>
2026-04-10 16:27:53 +0200

    genere 10 nombre aleatoires

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+module contenant une fonction qui utilise un async generator
+pour produire des nombres aléatoires
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    genere 10 fois un nombre entre 0 et 10
+    avec un delais de 1 seconde
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+
+        yield random.uniform(0, 10)
