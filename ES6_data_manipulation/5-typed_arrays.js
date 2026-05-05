97810678e67ecd37d385c5aca531b461c212fab4
loties <alexislaubert@yahoo.fr>
2026-05-05 15:16:02 +0200

    creation d'un ArrayBuffer avec DataView et Int8

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -5,8 +5,8 @@ export default function createInt8TypedArray (length, position, value) {
   if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
-
   view.setInt8(position, value);
 
   return view;
 }
+
