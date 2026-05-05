f62c8b2cd1b2a1fe497e4f60d1b3a6674a152fda
loties <alexislaubert@yahoo.fr>
2026-05-05 15:13:46 +0200

    Mise à jour des notes par ville avec filter et map

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,6 @@
-export default function createInt8TypedArray(length, position, value) {
-
+export default function createInt8TypedArray (length, position, value) {
   const buffer = new ArrayBuffer(length);
-  
+
   const view = new DataView(buffer);
   if (position >= length || position < 0) {
     throw new Error('Position outside range');
